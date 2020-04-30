180a99fdc34ad7d3dc2926e770e1a3e45cf39a56
Jessiedel Arnado <jessiedelarnado@gmail.com>
2020-04-30 19:04:39 +0800

    fixed token issue

--- backend-lumen/app/Http/Controllers/Auth/AuthController.php
+++ backend-lumen/app/Http/Controllers/Auth/AuthController.php
@@ -109,7 +109,7 @@ class AuthController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
-            'token' => $token,
+            'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => Auth::factory()->getTTL() * 60
         ], 200);
