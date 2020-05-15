5e8f6edb0a19dc1efee69082efd9c23a008bf3ba
Jessiedel Arnado <jessiedelarnado@gmail.com>
2020-05-15 17:27:22 +0800

    basic angular implementation using angular tour of heroes

--- frontend-angular/e2e/src/app.e2e-spec.ts
+++ frontend-angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('frontend-angular app is running!');
+    expect(page.getTitleText()).toEqual('angular-tour-of-heroes app is running!');
   });
 
   afterEach(async () => {
--- frontend-angular/src/app/app.component.spec.ts
+++ frontend-angular/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend-angular'`, () => {
+  it(`should have as title 'angular-tour-of-heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-angular');
+    expect(app.title).toEqual('angular-tour-of-heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('frontend-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heroes app is running!');
   });
 });
